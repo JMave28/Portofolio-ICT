ad3306a86fc8d541c0b0884cbdf16956770186ee
JMave28 <jasonmaverick@mutiarabangsa.sch.id>
2026-09-12 08:02:31 +0700

    Hubungkan portofolio ke Firebase Firestore

---
+++ temp_check.js
@@ -0,0 +1,21 @@
+
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    fontFamily: { sans: ['Inter', 'sans-serif'] },
+                    colors: {
+                        brand: {
+                            50: '#f0f5ff',
+                            100: '#e0ebff',
+                            500: '#3b82f6',
+                            600: '#2563eb',
+                            700: '#1d4ed8',
+                            900: '#1e3a8a'
+                        }
+                    }
+                }
+            }
+        }
+    
+;
